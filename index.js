8a29232c3d33204a065b9769559ba3ffdab06453
ayushmaannsingh <sa776857@gmail.com>
2025-10-20 01:18:48 +0530

    insert into user

--- index.js
+++ index.js
@@ -9,30 +9,45 @@
 
  });
 
- try {
-    connection.query("SHOW TABLES", (err, result) => {
-      if (err) throw err;
-      console.log(result);
-      
-   });
-
- } catch (err) {
-    console.log(err);
-    
- };
-
- connection.end();
-
-
   let getRandomUser = () => {
-  return {
-    Id: faker.string.uuid(),
-    username: faker.internet.username(),
-    email: faker.internet.email(),
-    password: faker.internet.password(),
+  return [
+     faker.string.uuid(),
+     faker.internet.username(),
+     faker.internet.email(),
+     faker.internet.password(),
    
-  };
+  ];
 }
 
 console.log(getRandomUser());
+
+ // Inserting New Data
+
+let q = "INSERT INTO `users` (id, username, email, password) VALUES ?";
+
+// let users = [
+//   ["123b", "1_ayushsingh", "ayush@gmail.comb", "singh"],
+//   ["123c", "2_suman", "sumansingh@gamil.comc", "rajput"],
+// ];
+
+let data = [];
+for(let i=1; i<=100; i++){
+ data.push(getRandomUser()); // 100 fake users
+}
+
+try {
+  connection.query(q, [data], (err, result) => {
+    if (err) throw err;
+    console.log(" Data inserted successfully!");
+    console.log(result);
+  });
+} catch (err) {
+  console.log(" Error:", err);
+}
+
+connection.end();
+
+
+
+ 
 
