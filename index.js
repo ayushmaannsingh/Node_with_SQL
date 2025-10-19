16f41b5d4eef9240fb004a548a181743bfe7ca31
ayushmaannsingh <sa776857@gmail.com>
2025-10-20 02:44:03 +0530

    add views

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
  const { faker } = require('@faker-js/faker');
  const mysql = require('mysql2');
+ const express = require("express");
+ const app = express(); 
+ const path = require ("path");
+
+ app.set("view engine", "ejs");
+ app.set("views", path.join(__dirname, "/views"));
 
  const connection = mysql.createConnection({
     host: 'localhost',
@@ -19,34 +25,54 @@
   ];
 }
 
-console.log(getRandomUser());
+// console.log(getRandomUser());
 
  // Inserting New Data
 
-let q = "INSERT INTO `users` (id, username, email, password) VALUES ?";
+// let q = "INSERT INTO `users` (id, username, email, password) VALUES ?";
 
 // let users = [
 //   ["123b", "1_ayushsingh", "ayush@gmail.comb", "singh"],
 //   ["123c", "2_suman", "sumansingh@gamil.comc", "rajput"],
 // ];
 
-let data = [];
-for(let i=1; i<=100; i++){
- data.push(getRandomUser()); // 100 fake users
-}
+// let data = [];
+// for(let i=1; i<=100; i++){
+//  data.push(getRandomUser()); // 100 fake users
+// }
 
-try {
-  connection.query(q, [data], (err, result) => {
+// try {
+//   connection.query(q, [data], (err, result) => {
+//     if (err) throw err;
+//     console.log(" Data inserted successfully!");
+//     console.log(result);
+//   });
+// } catch (err) {
+//   console.log(" Error:", err);
+// }
+
+// connection.end();
+
+app.get("/", (req, res) => {
+  let q = `SELECT count(*) FROM users`;
+  try {
+  connection.query(q, (err, result) => {
     if (err) throw err;
     console.log(" Data inserted successfully!");
-    console.log(result);
+    let count = (result[0] ["count(*)"]);
+    res.render("home.ejs", {count});
   });
 } catch (err) {
   console.log(" Error:", err);
+  res.send("some error in DB");
 }
+  
+}); 
 
-connection.end();
-
+app.listen("8080", () => {
+  console.log("server is listening to port 8080");
+  
+});
 
 
  
